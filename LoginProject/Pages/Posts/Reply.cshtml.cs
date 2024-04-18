43f29fa079bc322eae05161ff045080269f49260
František Šilhán <frantisek@slhn.cz>
2024-04-18 21:48:23 +0200

    add links to go through posts and replies

--- LoginProject/Pages/Posts/Reply.cshtml.cs
+++ LoginProject/Pages/Posts/Reply.cshtml.cs
@@ -28,16 +28,18 @@ namespace LoginProject.Pages.Posts {
                 return NotFound();
 
             Post = post;
-            if (Post.RootPostId == null) {
+            if (Post.RootPostId == null)
                 Post.RepliesCount = await _context.Posts.Where(p => p.RootPostId == Post.PostId).CountAsync();
-            } else {
+            else
                 Post.RepliesCount = await _context.Posts.Where(p => p.ParentPostId == Post.PostId).CountAsync();
-            }
 
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(Guid? id) {
+            if (!ModelState.IsValid)
+                return Page();
+
             if (id == null)
                 return NotFound();
 
@@ -48,13 +50,19 @@ namespace LoginProject.Pages.Posts {
             if (post == null)
                 return NotFound();
 
+            Guid rootPostId;
+            if (post.RootPostId != null)
+                rootPostId = (Guid)post.RootPostId;
+            else
+                rootPostId = post.PostId;
+
             Post reply = new Post {
                 PostId = Guid.NewGuid(),
                 AuthorId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!),
                 Content = Input.Content,
                 CreatedAt = DateTime.UtcNow,
                 ParentPostId = post.PostId,
-                RootPostId = post.RootPostId,
+                RootPostId = rootPostId,
             };
 
             await _context.Posts.AddAsync(reply);
